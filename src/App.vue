3352762069e0d1bd362e048db9c21d21c601edb6
zhanglainghao <15821881675@163.com>
2020-02-18 17:50:00 +0800

    add: 增加element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/home">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/page1">Page1</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/nestRouter/child1">NestRouter1</router-link>
-    </div> -->
-    <div class="content">
+    <!-- <div class="content">
       <div class="side">
         <Sidebar></Sidebar>
       </div>
@@ -15,19 +8,22 @@
         <Header></Header>
         <router-view />
       </div>
-    </div>
+    </div> -->
+    <Layout></Layout>
   </div>
 </template>
 
 <script>
 import { hanleRoutes } from "@/utils/permission";
 import { mapGetters } from "vuex";
-import Sidebar from "@/views/Sidebar/OriginSidebar.vue";
-import Header from "@/views/Header";
+// import Sidebar from "@/views/Sidebar/OriginSidebar.vue";
+// import Header from "@/views/Header";
+import Layout from "@/views/Layout";
 export default {
   components: {
-    Sidebar,
-    Header
+    // Sidebar,
+    // Header
+    Layout
   },
   data() {
     return {};
@@ -49,13 +45,15 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  height: 100vh;
   .content {
     height: 100%;
     display: flex;
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="layout">
+    <el-container class="layoutContent">
+      <el-aside width="200px">
+        Aside
+        <NavMenu></NavMenu>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <p>Main</p>
+          <router-view />
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import NavMenu from "./components/NavMenu";
+export default {
+  components: {
+    NavMenu
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.layout {
+  height: 100vh;
+  .layoutContent {
+    height: 100%;
+  }
+}
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  /* line-height: 200px; */
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  /* line-height: 160px; */
+}
+</style>
